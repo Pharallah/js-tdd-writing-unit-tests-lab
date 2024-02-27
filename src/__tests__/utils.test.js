95096d73d5706a88c0f668474014013ebbad1b82
Harry San Juan <harrycsanjuan@gmail.com>
2024-02-26 20:28:36 -0500

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
 // Your tests here
+
+//We'll give users double the amount of points if their word is a palindromeLinks to an external site.: a word that reads the same forwards and backwards, like "mom" or "racecar".
+
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true if the string is a palindrome, false if it isn't", () => {
+        const word = "RACECAR"
+        const truthiness = isPalindrome(word)
+        expect(truthiness).toBe()
+    })
+    it("returns false for an empty string", () => {
+        const word = ""
+        const truthiness = isPalindrome(word)
+        expect(truthiness).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+
+
+export function isPalindrome(word) {
+    const reversedWord = word.split("").reverse().join("")
+
+    // Tests case-insensitive palindrome
+    word.toLowerCase() === reversedWord.toLowerCase() ? true : false
+
+    // Tests for empty string
+    if (word === "") {
+        return false
+    }
+}
+
